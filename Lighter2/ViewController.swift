de7827442ce82db9b75082d5c2f9daddbc3676e4
Sergey Khvatov <skhvatov@ser.local>
2018-09-11 23:59:13 +0300

    Initial Commit

---
+++ Lighter2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lighter2
+//
+//  Created by Sergey Khvatov on 11/09/2018.
+//  Copyright © 2018 Sergey Khvatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
